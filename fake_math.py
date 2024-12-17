9d2b387bbb965c02aa28301440e2632df9680654
margonevolina <margo6984@gmail.com>
2024-12-17 22:58:46 +0800

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    return first/second
+
+# result1 = divide(69, 3)
+# result2 = divide(3, 0)
+# print(result1)
+# print(result2)
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return inf
+    return first/second
+# result3 = divide(49, 7)
+# result4 = divide(15, 0)
+# print(result3)
+# print(result4)
